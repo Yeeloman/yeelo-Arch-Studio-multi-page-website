b124de5dba2b19c0b075bd21b20a0e4e6f161b9f
Yeeloman <yami.onlyme@gmail.com>
2024-07-15 18:11:21 +0100

    add animation to the menu bar in mobile screens

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -5,6 +5,11 @@
 	import logo from '$assets/logo.svg';
 	import menuIcon from '$assets/shared/mobile/menu.svg';
 	import closeIcon from '$assets/shared/mobile/close.svg';
+	let isOpen = $state(false);
+
+	function toggleMenu() {
+		isOpen = !isOpen;
+	}
 	let { children } = $props();
 </script>
 
@@ -23,8 +28,17 @@
 			<Button variant="default" size="default" class="rounded-none">GET AN INVITE</Button>
 		</div>
 		<div class="hidden flex-col mymd:flex">
-			<Button variant="ghost" size="icon" class="relative ">
-				<img src={menu} alt="menu icon" />
+			<Button variant="ghost" size="icon" class="relative " onclick={toggleMenu}>
+				<!-- img src={menu} alt="menu icon" /-->
+				<div
+					class="absolute h-1 w-8 rounded bg-black transition-all duration-100 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-black before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-black after:transition-all after:duration-500 after:content-['']"
+					class:bg-transparent={isOpen}
+					class:rotate-[720deg]={isOpen}
+					class:before:translate-y-0={isOpen}
+					class:before:rotate-45={isOpen}
+					class:after:translate-y-0={isOpen}
+					class:after:-rotate-45={isOpen}
+				></div>
 			</Button>
 			<section
 				class="absolute left-0 top-20 hidden min-h-screen w-full flex-col items-center justify-center gap-9 bg-white mymd:flex"
